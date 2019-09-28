fe6fe63c156a78c3f16b3336a459c676b45b24c3
filipeamorais@pm.me <filipeamorais@pm.me>
2019-09-28 17:55:36 -0500

    add: added the second activity and preparing for the clicklistener.

--- app/src/main/java/com/example/secondassigment/MainActivity.java
+++ app/src/main/java/com/example/secondassigment/MainActivity.java
@@ -26,5 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 new ArrayAdapter<String>(
                         this, android.R.layout.simple_list_item_1, topicList);
         simpleList.setAdapter(myAdapter);
+        //clickListener that sends information to the other activity
+
     }
 }
