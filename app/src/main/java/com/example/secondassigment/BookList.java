8822dbc1d1c72695bcda3a9b1031ffac37c03535
filipeamorais@pm.me <filipeamorais@pm.me>
2019-10-02 18:41:30 -0500

    add: added the image and references for each book as a list in the activity booklist

--- app/src/main/java/com/example/secondassigment/BookList.java
+++ app/src/main/java/com/example/secondassigment/BookList.java
@@ -25,12 +25,15 @@ public class BookList extends AppCompatActivity {
         simpleListView = (ListView) findViewById(R.id.simpleListView);
 
         //adding items to the ArrayList(s) that will be displayed by the ListView according to the selection
-        androidList.add(new Item("Python Crash Course", R.drawable.pythoncrash));
-        androidList.add(new Item("Starting Out with Python", R.drawable.startingoutwithpython));
+        androidList.add(new Item("Android Studio 3.4 Development Essentials", R.drawable.androidstudioessential));
+        androidList.add(new Item("Android Programming: The Big Nerd Ranch Guide", R.drawable.androidprogramming));
 
-        mlList.add(new Item("Starting Out with Python", R.drawable.brazil_flag));
-        pythonList.add(new Item("Brazil", R.drawable.brazil_flag));
+        mlList.add(new Item("The Hundred-Page Machine Learning Book", R.drawable.hundredpageml));
+        mlList.add(new Item("Hands-On Machine Learning with Scikit-Learn and TensorFlow: Concepts, Tools, and Techniques to Build Intelligent Systems", R.drawable.handonsmachinelearning));
 
+        pythonList.add(new Item("Learning Python", R.drawable.learningpython));
+        pythonList.add(new Item("Python Crash Course", R.drawable.pythoncrash));
+        pythonList.add(new Item("Starting Out with Python", R.drawable.startingoutwithpython));
 
     }
 }
