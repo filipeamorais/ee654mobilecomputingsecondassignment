a212c69aa0f5964865eed4b83c08545f3469394e
filipeamorais@pm.me <filipeamorais@pm.me>
2019-10-03 10:53:21 -0500

    add: able to receive the intent from the previous activity and now able to display one of the information about it on the third activity.

--- app/src/main/java/com/example/secondassigment/BookDisplay.java
+++ app/src/main/java/com/example/secondassigment/BookDisplay.java
@@ -2,7 +2,9 @@ package com.example.secondassigment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BookDisplay extends AppCompatActivity {
 
@@ -11,6 +13,12 @@ public class BookDisplay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_display);
 
-        
+        //getting the intend and checking its selection
+        Intent intent = getIntent();
+        Item bookChosen =
+                (Item) intent.getSerializableExtra(BookList.MESSAGE);
+        TextView textView = (TextView)
+                findViewById(R.id.textView);
+        textView.setText(bookChosen.getFlagName());
     }
 }
