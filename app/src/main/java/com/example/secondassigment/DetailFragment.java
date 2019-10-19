370b61b2bf56aecfaba6b0cf961e085cd99b9c2b
filipeamorais@pm.me <filipeamorais@pm.me>
2019-10-19 16:18:25 -0500

    add: added code to save argument in the bundle to send from the activity to the fragment but there are errors on the findViewById

--- app/src/main/java/com/example/secondassigment/DetailFragment.java
+++ app/src/main/java/com/example/secondassigment/DetailFragment.java
@@ -1,7 +1,19 @@
 package com.example.secondassigment;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+@SuppressLint({"NewApi", "ValidFragment"})
 class DetailFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.picture_fragment, container, false);
+        return view;
+    }
 
 }
--- app/src/main/java/com/example/secondassigment/PictureFragment.java
+++ app/src/main/java/com/example/secondassigment/PictureFragment.java
@@ -1,5 +1,6 @@
 package com.example.secondassigment;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,10 +9,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+@SuppressLint({"NewApi", "ValidFragment"})
 class PictureFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.picture_fragment, container, false);
+        return view;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        int bookImageNumber = getArguments().getInt("book_image");
+        setImage(bookImageNumber);
+    }
+
+    public void setImage (int bookImageNumber) {
+        ImageView imageView = (ImageView) findViewById(R.id.bookImage);
+        imageView.setImageResource(bookImageNumber);
+    }
+
+
 }
